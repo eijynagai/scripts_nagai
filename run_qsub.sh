d9f37859bb619c84007ff6e1219097901ffb324f
eijynagai <eijynagai@gmail.com>
2020-03-30 12:13:38 +0900

    include duration information

--- run_qsub.sh
+++ run_qsub.sh
@@ -1,19 +1,34 @@
 #!/bin/env sh
-#$ -l <resource>
-#$ -cwd                       #job is located in the current wd
-#$ -e <path>                  #saves the execution result, std error
-#$ -jy                        #merges the contents of standard error with ouput
-#$ -N <job name> 
-#$ -o <path>                  #saves the execution result, std out
 #$ -S /bin/sh
-##$ -pe <name>                #parallel env name, use "def_slot 4"
-##$ -masterl <resource>       #use this or l
+#$ -cwd                       #job is located in the current wd
+#$ -jy                        #merges the contents of standard error with ouput
 ##$ -hold_jid <job_id>        #holds the jov until job_id ends
 #$ -R y                       #reserve resources until have enough to start
-#$ -M <email contact>
-#$ -m bae                     #in what circunstance beg/abort/end/suspension
 #$ -v LD_LIBRARY_PATH="",PATH=$PATH      #export these environmental variables 
+##$ -masterl <resource>       #use this or l
+##$ -pe <name>                #parallel env name, use "def_slot 4"
+##$ -M <your_email>
+##$ -m bae                     #in what circunstance beg/abort/end/suspension
+
+## modify below
+#$ -l os7                     #run only os7
+#$ -l s_vmem=8G
+#$ -l mem_req=8G
+#$ -l <resource>              #opt: ljob,lmem,mjob check qfree
+#$ -e <path>                  #saves the execution result, std error
+#$ -o <path>                  #saves the execution result, std out
+#$ -N <job name> 
 
 echo "Hello world"
+
+
+### Duration information
+##echo "START: " `date  "+%Y%m%d-%H%M%S"`
+echo "DONE: " `date  "+%Y%m%d-%H%M%S"`
+#start_time=`date +%s`
+end_time=`date +%s`
+duration=$((end_time - start_time))
+echo "TIME: " $duration
+
 
 exit;
