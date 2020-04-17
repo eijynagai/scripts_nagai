97ff73622c679389db4c7592f7d6ba2035ed1639
eijynagai <eijynagai@gmail.com>
2020-04-17 21:42:12 +0900

    include function reverse complementary sequence

--- nagai_seqs.py
+++ nagai_seqs.py
@@ -15,10 +15,26 @@ Last modification: 20200417: initial commit
 def atcg(s):
     return s.count('A'), s.count('T'), s.count('C'), s.count('G')
 
+
 # transcribing DNA to RNA
 def dna2rna(t):
     # replace T to U
     return t.replace('T','U')
 
-seq='GATGGAACTTGACTACGTAAATT'
-print(dna2rna(seq))
+
+# reverse complementary strand
+def revcomp(seq):
+    complement = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    return "".join(complement[base] for base in seq)[::-1]
+
+
+
+def main():
+    # runing tests on the functions
+    seq='AAAACCCGGT'
+    print(revcomp(seq))
+
+
+
+if __name__ == '__main__':
+    main()
