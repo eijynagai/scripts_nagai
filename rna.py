b314235735cd470d750faa0e5b23c229dbd421ec
eijynagai <eijynagai@gmail.com>
2022-02-28 17:24:09 +0900

    updated script

--- rna.py
+++ rna.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author : eijy <eijy@localhost>
-Date   : 2022-02-22
+Date   : 2022-02-28
 Purpose: Transcribe DNA to RNA
 """
 
@@ -11,12 +11,8 @@ from typing import NamedTuple, TextIO
 
 class Args(NamedTuple):
     """ Command-line arguments """
-    positional: str
-    string_arg: str
-    int_arg: int
-    file: TextIO
-    on: bool
-
+    files: List[TextIO]
+    out_dir: str
 
 # --------------------------------------------------
 def get_args() -> Args:
@@ -37,12 +33,12 @@ def get_args() -> Args:
                         help='Input DNA file(s)',
                         metavar='FILE',
                         nargs='+',
-                        types=argparse.FileType('rt'))
+                        type=argparse.FileType('rt'))
 
 
     args = parser.parse_args()
 
-    return Args(args.positional, args.arg, args.int, args.file, args.on)
+    return Args(files=args.file, out_dir=args.out_dir)
 
 
 # --------------------------------------------------
@@ -50,18 +46,18 @@ def main() -> None:
     """ Make a jazz noise here """
 
     args = get_args()
-    str_arg = args.string_arg
-    int_arg = args.int_arg
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    
+    if not os.path.isdir(args.out_dir):
+        os.makedirs(args.out_dir)
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
-
+    num_files, num_seqs = 0, 0
+    for fh in args.files:
+    # open an output file in the output directory
+    
+    # for each line/sequence from the input file:
+        # write the transcribed sequence to the output file
+        # update the number of files processed
+    print('Done!')
 
 # --------------------------------------------------
 if __name__ == '__main__':
